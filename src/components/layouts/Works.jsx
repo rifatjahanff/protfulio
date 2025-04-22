e19df7ff746154c809b4d21ed377c069ffe6fb43
rifatjahanff <jahanrifat953@gmail.com>
2025-04-22 18:24:04 +0600

    somthing

--- src/components/layouts/Works.jsx
+++ src/components/layouts/Works.jsx
@@ -12,16 +12,20 @@ const Works = () => {
 
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 mt-[70px]">
         <a href="https://react-orbi.vercel.app/" target="_blank" rel="noopener noreferrer">
-          <div className="bg-[url('/src/assets/orabi.jpg')] bg-cover bg-center bg-no-repeat w-full h-[300px] border-[2px] border-amber-400"></div>
+          <div className="bg-[url('/src/assets/orabi.jpg')] bg-cover bg-center bg-no-repeat w-full h-[300px] border-[2px] border-amber-400 rounded-2xl"></div>
+          <p className="text-white text-2xl mt-2 text-center">Made By React</p>
         </a>
-        <a href="https://react-orbi.vercel.app/" target="_blank" rel="noopener noreferrer">
-          <div className="w-full h-[300px] border-[2px] border-amber-400"></div>
+        <a href="https://react-first-projectt.vercel.app" target="_blank" rel="noopener noreferrer">
+          <div className="bg-[url('/src/assets/WorkTwo.jpg')] bg-cover bg-center bg-no-repeat w-full h-[300px] border-[2px] border-amber-400 rounded-2xl"></div>
+          <p className="text-white text-2xl mt-2 text-center">Made By React</p>
         </a>
-        <a href="https://react-orbi.vercel.app/" target="_blank" rel="noopener noreferrer">
-          <div className="w-full h-[300px] border-[2px] border-amber-400"></div>
+        <a href="https://add-to-cart-five-blue.vercel.app/" target="_blank" rel="noopener noreferrer">
+          <div className="bg-[url('/src/assets/WorkThree.jpg')] bg-cover bg-center bg-no-repeat w-full h-[300px] border-[2px] border-amber-400 rounded-2xl"></div>
+          <p className="text-white text-2xl mt-2 text-center">Made By React</p>
         </a>
-        <a href="https://react-orbi.vercel.app/" target="_blank" rel="noopener noreferrer">
-          <div className="w-full h-[300px] border-[2px] border-amber-400"></div>
+        <a href="https://react2nd-project.vercel.app" target="_blank" rel="noopener noreferrer">
+          <div className="bg-[url('/src/assets/WorkFour.png')] bg-cover bg-center bg-no-repeat w-full h-[300px] border-[2px] border-amber-400 rounded-2xl"></div>
+          <p className="text-white text-2xl mt-2 text-center">Made By React</p>
         </a>
       </div>
     </div>
